dcbcee0cf520f473eba359e9e14c62e73e01eaac
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-08-17 03:20:28 +0000

    Adding new version of puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,8 @@
 # Install Nginx web server with custom header response
-exec { '/usr/bin/env apt-get update -y': }
-exec { '/usr/bin/env apt-get install nginx -y': }
-exec { '/usr/bin/env header="\\\tadd_header X-Served-By $HOSTNAME;\n"': }
-exec { '/usr/bin/env sudo sed -i "38i $header" /etc/nginx/sites-available/default': }
-exec { '/usr/bin/env sudo service nginx restart':}
+exec { 'Nginx install with custom header response':
+command => 'sudo apt-get update -y && sudo apt-get install nginx -y && 
+            header="\\\tadd_header X-Served-By $HOSTNAME;\n" && 
+            sudo sed -i "38i $header" /etc/nginx/sites-available/default && 
+            sudo service nginx restart',
+provider => shell,
+}
