690eaa7154c9660dd184c7f1f99a65b14e14eea2
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-17 04:44:01 +0000

    Fixing method definition

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,18 @@ from sys import argv
 from requests import get
 
 
-if __name__ == "__main__":
+def get_data_api(user_id):
     done = []
     url = "https://jsonplaceholder.typicode.com/users"
-    user = get(url + "/{}".format(argv[1])).json()
-    tasks = get(url + "/{}/todos".format(argv[1])).json()
+    user = get(url + "/{}".format(user_id)).json()
+    tasks = get(url + "/{}/todos".format(user_id)).json()
     for task in tasks:
         if task["completed"]:
             done.append(task.get("title"))
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user.get('name'), len(done), len(tasks)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user["name"], len(done), len(tasks)))
     for task in done:
-        print('\t {}'.format(task))
+        print("\t {}".format(task))
+
+if __name__ == "__main__":
+    get_data_api(int(argv[1]))
