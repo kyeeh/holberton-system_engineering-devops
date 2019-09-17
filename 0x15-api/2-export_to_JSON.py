6d7411425d61695c009d873ca8cfd72f601f36a6
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-17 21:16:23 +0000

    Adding code for Task#02

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 # using this REST API, for a given employee ID,
-# returns information about his/her TODO list progress.
-from sys import argv
+# returns information about his/her TODO list progress
+# to export data in the JSON format.
+import json
 from requests import get
+from sys import argv
 
+
+def api_to_json(user_id):
+    task_list = []
+    url = "https://jsonplaceholder.typicode.com/"
+    user = get(url + "users/{}".format(user_id)).json()
+    tasks = get(url + "todos?userId={}".format(user_id)).json()
+    for task in tasks:
+        tdict = {}
+        tdict["task"] = task.get("title")
+        tdict["completed"] = task.get("completed")
+        tdict["username"] = user.get("username")
+        task_list.append(tdict)
+    with open("{}.json".format(user_id), 'w', newline='') as json_file:
+        json.dump({user_id: task_list}, json_file)
 
 if __name__ == "__main__":
-    done = []
-    url = "https://jsonplaceholder.typicode.com/users"
-    user = get(url + "/{}".format(argv[1])).json()
-    tasks = get(url + "/{}/todos".format(argv[1])).json()
-    for task in tasks:
-        if task["completed"]:
-            done.append(task.get("title"))
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user.get('name'), len(done), len(tasks)))
-    for task in done:
-        print('\t {}'.format(task))
+    api_to_json(int(argv[1]))
