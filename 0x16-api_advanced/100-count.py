8d128a956772cd6773231e7ef33756276bed4bdc
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-20 03:36:32 +0000

    Adding second value for sorted counters

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -28,9 +28,9 @@ def count_words(subreddit, word_list, next="", counters={}):
             count_words(subreddit, word_list, next, counters)
         else:
             sorted_counters = sorted(counters.items(
-                ), key=lambda t: (t[1]), reverse=True)
+                ), key=lambda t: (t[1], t[0]), reverse=True)
             for (key, value) in sorted_counters:
                 if value != 0:
                     print('{}: {}'.format(key, value))
     except Exception:
-        print()
+        return (None)
