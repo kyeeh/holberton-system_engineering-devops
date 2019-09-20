7712561e1a17eb186be02c2ece18e2d8699a656c
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-20 14:58:03 +0000

    Hot posts by subreddit in a recursive way with sorted count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,7 +4,7 @@ Recursive function that queries the Reddit API, parses the title of all hot
 articles, and prints a sorted count of given keywords (case-insensitive,
 delimited by spaces.
 """
-from requests import get, HTTPError
+from requests import get
 
 
 def count_words(subreddit, word_list, next="", counters={}):
@@ -14,17 +14,17 @@ def count_words(subreddit, word_list, next="", counters={}):
     headers = {"user-agent": "kyeeh"}
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, next)
     if len(counters) == 0:
+        word_list = list(set(word_list))
         for word in word_list:
             counters[word] = 0
-    req = get(url, headers=headers, allow_redirects=False)
-    if req.status_code == 200:
-        req_data = req.json()
+    try:
+        req_data = get(url, headers=headers, allow_redirects=False).json()
         hot_posts = req_data["data"]["children"]
         for post in hot_posts:
             for key in counters:
-                counters[key] += post['data']['title'].lower(
+                counters[key] += post["data"]["title"].lower(
                     ).split(' ').count(key.lower())
-        next = req_data['data']['after']
+        next = req_data["data"]["after"]
         if next:
             count_words(subreddit, word_list, next, counters)
         else:
@@ -33,3 +33,5 @@ def count_words(subreddit, word_list, next="", counters={}):
             for (key, value) in sorted_counters:
                 if value != 0:
                     print('{}: {}'.format(key, value))
+    except Exception:
+        return (None)
