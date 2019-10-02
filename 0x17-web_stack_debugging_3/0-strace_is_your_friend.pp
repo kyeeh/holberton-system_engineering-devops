f40a4f11a3fe82c49abbc5cff91d9d647c4c1bb2
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-10-02 23:52:33 +0000

    Adding full path for linux commands

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Puppet manifest to fix misconfiguration in Wordpress
 exec { 'no phpp':
-    command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
+    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
 }
 exec {'restart web server':
-  command => 'service apache2 restart'
+  command => '/usr/sbin/service apache2 restart'
 }
