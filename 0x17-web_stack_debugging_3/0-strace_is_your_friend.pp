e0f03c610a450af6113dee6b645120a63f94216b
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-10-02 23:46:05 +0000

    Testing with config update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,3 @@
 exec { 'no phpp':
-    command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
-}
-exec {'restart web server':
-  command => '/usr/sbin/service apache2 restart',
+    command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
 }
