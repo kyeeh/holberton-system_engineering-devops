f04c3b517be327099e968ed5300cada8589cb3a0
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-10-11 03:40:46 +0000

    Adding puppet file to fix worker processes in Nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to fix nginx worker processes.
 exec { 'workers':
-    command => '/bin/sed -i "s/15/4000/g" /etc/default/nginx'
+    command => '/bin/sed -i "s/15/8000/g" /etc/default/nginx'
 }
 exec {'restart web server':
   command => '/usr/sbin/service nginx restart'
